a11706e55a986121ee75ba952d8db62ed1977745
szincone <sawyerzincone@gmail.com>
2019-04-14 01:09:35 -0600

    ran migrations

--- api/db/migrations/20190413223253_coin_data.js
+++ api/db/migrations/20190413223253_coin_data.js
@@ -1,8 +1,19 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function generateDBschema(knex) {
+  return knex.schema.createTable('Coins', (table) => {
+    table.increments('id').primary();
+    table.string('symbol').notNullable();
+    table.string('name', 120).notNullable();
+    table.integer('market_cap').notNullable();
+    table.integer('usd_price').notNullable();
+    table.integer('btc_price').notNullable();
+    table.integer('circulation').notNullable();
+    table.integer('volume').notNullable();
+    table.integer('fluctuation_hour').notNullable();
+    table.integer('fluctuation_day').notNullable();
+    table.integer('fluctuation_week').notNullable();
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function rollBackMigrations(knex) {
+  return knex.schema.dropTableIfExists('Coins');
 };
