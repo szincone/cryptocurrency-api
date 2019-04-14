27ce89dabbd237c68427967d2cf8b426657f8c56
szincone <sawyerzincone@gmail.com>
2019-04-13 22:34:39 -0600

    basic server running, installed dependencies

---
+++ api/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+// const configureMiddleware = require('./middleware/middleware.js');
+
+const server = express();
+
+// configureMiddleware(server);
+
+const PORT = process.env.PORT || 9000;
+
+server.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
