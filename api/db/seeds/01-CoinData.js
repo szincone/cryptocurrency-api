6fdbb71cabf8397cfd15c8478d642d6baf4490a8
szincone <sawyerzincone@gmail.com>
2019-04-20 00:58:13 -0600

    refactored seeding loop

--- api/db/seeds/01-CoinData.js
+++ api/db/seeds/01-CoinData.js
@@ -13,30 +13,14 @@ exports.seed = async function insertCoinData(knex) {
   const coinDataRes = await getCoinData();
   try {
     await knex('Coins').truncate();
-    let [startSlice, stopSlice, counter] = [1, 150, coinDataRes.length];
-    // console.log('LALA', startSlice, stopSlice, 'end slice', counter);
+    let [startSlice, stopSlice, counter] = [0, 50, coinDataRes.length];
     while (counter > 0) {
+      // eslint-disable-next-line no-await-in-loop
       await knex('Coins').insert(coinDataRes.slice(startSlice, stopSlice));
-      startSlice += 150;
-      stopSlice += 150;
-      counter -= 150;
+      startSlice += 50;
+      stopSlice += 50;
+      counter -= 50;
     }
-    // await knex('Coins').insert(coinDataRes.slice(1, 150));
-    // await knex('Coins').insert(coinDataRes.slice(150, 300));
-    // await knex('Coins').insert(coinDataRes.slice(300, 450));
-    // if (coinDataRes.length - 450 <= coinDataRes.length) {
-    //   await knex('Coins').insert(coinDataRes.slice(450, 600));
-    // } else if (
-    //   coinDataRes.length - 600 <= coinDataRes.length &&
-    //   coinDataRes.length - 600 > 0
-    // ) {
-    //   await knex('Coins').insert(coinDataRes.slice(600, 750));
-    // } else if (
-    //   coinDataRes.length - 750 <= coinDataRes.length &&
-    //   coinDataRes.length - 750 > 0
-    // ) {
-    //   await knex('Coins').insert(coinDataRes.slice(750));
-    // }
   } catch (err) {
     throw err;
   }
